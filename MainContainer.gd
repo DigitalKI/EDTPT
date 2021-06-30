296de588316bec2d4132c8ea73e0ef05ffbe1273
Toshiwoz <toshiwo.a@gmail.com>
2021-06-29 21:13:27 -0500

    Transparency

--- MainContainer.gd
+++ MainContainer.gd
@@ -3,6 +3,7 @@ extends HBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	get_tree().get_root().set_transparent_background(true)
 	pass # Replace with function body.
 
 
--- TabScreens/Ships.gd
+++ TabScreens/Ships.gd
@@ -4,6 +4,9 @@ var ship_button_res = preload("res://TabScreens/ShipButton.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func initialize_ships_tab():
+	for shipb in $MarginContainer/HBoxContainer/ScrollContainer/VBoxContainer.get_children():
+		shipb.queue_free()
+
 	for ship in data_reader.ships_manager.current_stored_ships:
 		var ship_dct : Dictionary = data_reader.ships_manager.current_stored_ships[ship]
 		print(data_reader.ships_manager.current_stored_ships[ship])
