35e3e41dbef89427c08dc33451c7ffcbfe2605e3
Toshiwoz <toshiwo.a@gmail.com>
2021-09-16 18:41:57 -0500

    separating functions

--- TabScreens/QueryBuilder/QueryBuilder.gd
+++ TabScreens/QueryBuilder/QueryBuilder.gd
@@ -7,20 +7,75 @@ onready var query_view: TextEdit = $HBoxContainer/TabContainer/QueryView/Resulti
 onready var selected_events : ItemList = $HBoxContainer/PanelResult/SelectedEvents
 
 var tree_root : TreeItem
-var color_coords_bg : Color = Color("#ff7802")
-var color_coords_fg : Color = Color("#230089")
-var color_sys_bg : Color = Color("#0033cc")
-var color_sys_fg : Color = Color("#ff7802")
+export(Color) var coords_bg : Color = Color("#ff7802")
+export(Color) var coords_fg : Color = Color("#230089")
+export(Color) var system_bg : Color = Color("#0033cc")
+export(Color) var system_fg : Color = Color("#ff7802")
+export(Color) var selected_field_bg : Color = Color("#d5d5d5")
+export(Color) var selected_field_fg : Color = Color("#3a3a3a")
+var field_bg : Color = Color("#d5d5d5")
+var field_fg : Color = Color("#3a3a3a")
 
-var built_query : Dictionary = {}
+var query_structure : Dictionary = {}
 
 func _ready():
 	pass
 
+func _on_EventTypes_item_activated(index):
+	if selected_events.get_item_count() < 10:
+		var selected_event_type := event_types_table.get_item_text(index)
+		var selected_event_color := event_types_table.get_item_custom_bg_color(index)
+		selected_events.add_item(selected_event_type)
+		selected_events.set_item_custom_bg_color(selected_events.get_item_count() - 1, selected_event_color)
+		show_events_fields(selected_event_type)
+		event_tabs.tabs_visible = 1
+
+func _on_BtRemoveAll_pressed():
+	selected_events.clear()
+	query_structure.clear()
+
+func _on_EventFields_item_activated():
+	var selected_item : TreeItem = events_fields.get_selected()
+	if selected_item.get_children():
+		selected_item.collapsed = !selected_item.collapsed
+	elif build_query_structure(selected_item) == "added":
+		selected_item.set_custom_bg_color(0, selected_field_bg)
+		selected_item.set_custom_color(0, selected_field_fg)
+	else:
+		selected_item.set_custom_bg_color(0, selected_field_fg)
+		selected_item.set_custom_color(0, selected_field_bg)
+	query_view.text = query_structure_to_select()
+
+func build_query_structure(_selected_field : TreeItem):
+	var result = ""
+	if _selected_field.get_text(0).length() > 0:
+		var event_type_item : TreeItem = _selected_field.get_parent()
+		if event_type_item.get_text(0):
+			if !query_structure.has(event_type_item.get_text(0)):
+				query_structure[event_type_item.get_text(0)] = []
+			if query_structure[event_type_item.get_text(0)].has(_selected_field.get_text(0)):
+				query_structure[event_type_item.get_text(0)].erase(_selected_field.get_text(0))
+				result = "removed"
+			else:
+				query_structure[event_type_item.get_text(0)].append(_selected_field.get_text(0))
+				result = "added"
+	return result
+
+func query_structure_to_select():
+	var resulting_query : String = "SELECT"
+	var tables_query := ""
+	var fields_query := ""
+	for tbl in query_structure.keys():
+		for fld in query_structure[tbl]:
+			fields_query += ", %s" % fld
+		tables_query += ", %s" % tbl
+	resulting_query += fields_query.trim_prefix(",") + " FROM " + tables_query.trim_prefix(",")
+	return resulting_query
+
 func initialize_builder():
 	list_all_tables()
 	initialize_event_fields()
-	
+
 func list_all_tables():
 	event_types_table.clear()
 	selected_events.clear()
@@ -29,19 +84,19 @@ func list_all_tables():
 	var event_tables_system_addr : Array = data_reader.dbm.get_all_event_tables(" AND SQL LIKE '%SystemAddress%'")
 	for sys_tbl in event_tables_coords:
 		event_types_table.add_item(sys_tbl)
-		event_types_table.set_item_custom_bg_color(event_types_table.get_item_count() - 1, color_coords_bg)
-		event_types_table.set_item_custom_fg_color(event_types_table.get_item_count() - 1, color_coords_fg)
+		event_types_table.set_item_custom_bg_color(event_types_table.get_item_count() - 1, coords_bg)
+		event_types_table.set_item_custom_fg_color(event_types_table.get_item_count() - 1, coords_fg)
 	for addr_tbl in event_tables_system_addr:
 		if !event_tables_coords.has(addr_tbl):
 			event_types_table.add_item(addr_tbl)
-			event_types_table.set_item_custom_bg_color(event_types_table.get_item_count() - 1, color_sys_bg)
-			event_types_table.set_item_custom_fg_color(event_types_table.get_item_count() - 1, color_sys_fg)
+			event_types_table.set_item_custom_bg_color(event_types_table.get_item_count() - 1, system_bg)
+			event_types_table.set_item_custom_fg_color(event_types_table.get_item_count() - 1, system_fg)
 	for tbl in event_tables:
 		if !event_tables_coords.has(tbl) && !event_tables_system_addr.has(tbl):
 			event_types_table.add_item(tbl)
 
 func initialize_event_fields():
-	built_query.clear()
+	query_structure.clear()
 	events_fields.clear()
 	tree_root = events_fields.create_item()
 	events_fields.set_column_titles_visible(true)
@@ -50,6 +105,8 @@ func initialize_event_fields():
 	events_fields.set_column_expand(0, true)
 	events_fields.set_column_title(1, "Type")
 	events_fields.set_column_min_width(1, 40)
+	field_bg = tree_root.get_custom_bg_color(0)
+	field_fg = tree_root.get_custom_color(0)
 
 func show_events_fields(_event_name : String):
 	var table_item : TreeItem = events_fields.create_item(tree_root)
@@ -63,37 +120,5 @@ func show_events_fields(_event_name : String):
 		var type_item : TreeItem = events_fields.create_item(table_item)
 		type_item.set_text(0, fld["name"])
 		type_item.set_text(1, fld["type"])
-
-func _on_EventTypes_item_activated(index):
-	if selected_events.get_item_count() < 10:
-		var selected_event_type := event_types_table.get_item_text(index)
-		var selected_event_color := event_types_table.get_item_custom_bg_color(index)
-		selected_events.add_item(selected_event_type)
-		selected_events.set_item_custom_bg_color(selected_events.get_item_count() - 1, selected_event_color)
-		show_events_fields(selected_event_type)
-		event_tabs.tabs_visible = 1
-
-
-func _on_BtRemoveAll_pressed():
-	selected_events.clear()
-
-
-func _on_EventFields_item_activated():
-	var resulting_query : String = "SELECT"
-	var selected_item : TreeItem = events_fields.get_selected()
-	if selected_item.get_children():
-		selected_item.collapsed = !selected_item.collapsed
-	elif selected_item.get_text(0).length() > 0:
-		var item_parent : TreeItem = selected_item.get_parent()
-		if item_parent.get_text(0):
-			if !built_query.has(item_parent.get_text(0)):
-				built_query[item_parent.get_text(0)] = []
-			built_query[item_parent.get_text(0)].append(selected_item.get_text(0))
-	var tables_query := ""
-	var fields_query := ""
-	for tbl in built_query.keys():
-		for fld in built_query[tbl]:
-			fields_query += ", %s" % fld
-		tables_query += ", %s" % tbl
-	resulting_query += fields_query.trim_prefix(",") + " FROM " + tables_query.trim_prefix(",")
-	query_view.text = resulting_query
+		type_item.set_custom_bg_color(0, selected_field_fg)
+		type_item.set_custom_color(0, selected_field_bg)
